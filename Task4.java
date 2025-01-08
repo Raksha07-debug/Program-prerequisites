13fd9613a011e5c7adad9e4e6fcca2d454a97f2e
Raksha <rak070803@gmail.com>
2025-01-08 11:03:22 +0530

    task4

---
+++ Task4.java
@@ -0,0 +1,12 @@
+ import java.util.*;
+class Task4{
+public static void main(String args[]){
+Scanner sc= new Scanner(System.in);
+System.out.println("enter radius of a circle");
+ double r= sc.nextDouble();
+double a=3.14*r*r;
+System.out.println("Area of a circle: " + a);
+
+
+}
+}
