33ab18d77578441466a7964dd1c34e1ad2eb1bbc
Raksha <rak070803@gmail.com>
2025-01-08 11:30:38 +0530

    Task7

---
+++ Task7.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Task7{
+public static void main(String args[]){
+Scanner sc= new Scanner(System.in);
+System.out.println("enter the value for length");
+float length=sc.nextFloat();
+System.out.println("enter the value for width");
+float width=sc.nextFloat();
+float perimeter=2*(length+width);
+System.out.println("perimeter of a rectangle: "+perimeter);
+
+
+
+
+}
+}
