f12c951d08201f2545409129c64c0995a7600e6f
Raksha <rak070803@gmail.com>
2025-01-08 10:41:00 +0530

    task 2

---
+++ Task2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Task2 {
+public static void main(String args[]){
+Scanner sc =new Scanner(System.in);
+System.out.println("enter two numbers");
+int a=sc.nextInt();
+int b= sc.nextInt();
+System.out.println("Addition of number: "+ a+b);
+
+
+}
+}
